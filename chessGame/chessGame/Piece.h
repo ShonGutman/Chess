d2feee968b8b58caf23019de7ad6dfd9c711f671
Shon Gutman <gshon2007@gmail.com>
2023-12-23 18:41:58 +0200

    add ctor, setters in class piece

--- chessGame/chessGame/Piece.h
+++ chessGame/chessGame/Piece.h
@@ -19,11 +19,19 @@
 
 #define EMPTY_SQUARE '#'
 
+constexpr int BOARD_SIZE = 8;
+
 class Board;
 
 class Piece
 {
 public:
+	/*
+	* constructor for Piece
+	* @param color - color of chess piece
+	* @param type - piece type
+	* @param pos - square of piece
+	*/
 	Piece(const char color, const char type, const Square& pos);
 
 	virtual ~Piece();
@@ -37,6 +45,8 @@ public:
 	Square& getPosition() const;
 	std::vector<Square> getMoves() const;
 
+
+
 	void clearVector();
 	
 	bool isLegalMove(const Square& moveTo) const;
@@ -48,4 +58,28 @@ protected:
 	char _type;
 	Square _position;
 	std::vector<Square> _possibleMoves;
+
+
+private:
+
+	/*
+	* set the color of the piece
+	*
+	* @param color - color of chess piece
+	*/
+	void setColor(const char color);
+
+	/*
+	* set type of the piece
+	* 
+	* @param type - piece type
+	*/
+	void setType(const char type);
+
+	/*
+	* set square of the piece
+	* 
+	* @param pos - square of piece
+	*/
+	void setPosition(const Square& pos);
 };
--- chessGame/chessGame/Square.h
+++ chessGame/chessGame/Square.h
@@ -56,6 +56,9 @@ public:
 	*/
 	bool operator==(const Square& other) const;
 
+	int getX() const;
+	int getY() const;
+
 private:
 	int _x;
 	int _y;
