c5389558b8558ed00e8596d7c05afcc4e2d01cb8
daniel <danie.zinger@gmail.com>
2023-12-30 18:52:34 +0200

    frontend changes

--- chessGame/chessGame/Source.cpp
+++ chessGame/chessGame/Source.cpp
@@ -17,7 +17,7 @@ using std::string;
 
 int main()
 {
-	srand(time_t(NULL));
+	/*srand(time_t(NULL));
 	try
 	{
 		Game chessGame;
@@ -41,75 +41,66 @@ int main()
 	{
 		std::cerr << "Error: It's ok, I don't understand chess or what you've done" << endl;
 		return -1;
+	}*/
+
+	Pipe p;
+	bool isConnect = p.connect();
+	Game chessGame;
+	
+	string ans;
+	while (!isConnect)
+	{
+		cout << "cant connect to graphics" << endl;
+		cout << "Do you try to connect again or exit? (0-try again, 1-exit)" << endl;
+		std::cin >> ans;
+		if (ans == "0")
+		{
+			cout << "trying connect again.." << endl;
+			Sleep(5000);
+			isConnect = p.connect();
+		}
+		else 
+		{
+			p.close();
+			return -1;
+		}
 	}
-	//Pipe p;
-	//bool isConnect = p.connect();
-	//Game chessGame;
-	//
-	//string ans;
-	//while (!isConnect)
-	//{
-	//	cout << "cant connect to graphics" << endl;
-	//	cout << "Do you try to connect again or exit? (0-try again, 1-exit)" << endl;
-	//	std::cin >> ans;
-
-	//	if (ans == "0")
-	//	{
-	//		cout << "trying connect again.." << endl;
-	//		Sleep(5000);
-	//		isConnect = p.connect();
-	//	}
-	//	else 
-	//	{
-	//		p.close();
-	//		return -1;
-	//	}
-	//}
-	//
-
-	//char msgToGraphics[1024];
-	//// msgToGraphics should contain the board string accord the protocol
-	//// YOUR CODE
-
-	//strcpy_s(msgToGraphics, "r###k##r"
-	//						"########"
-	//						"########"
-	//						"########"
-	//						"########"
-	//						"########"
-	//						"########"
-	//						"R###K##R0"); // just example...
-	//
-	//p.sendMessageToGraphics(msgToGraphics);   // send the board string
-
-	//// get message from graphics
-	//string msgFromGraphics = p.getMessageFromGraphics();
-
-	//while (msgFromGraphics != "quit")
-	//{
-	//	// should handle the string the sent from graphics
-	//	// according the protocol. Ex: e2e4           (move e2 to e4)
-	//	
-	//	// YOUR CODE
-	//	//const char code[2] = {char( '0' + chessGame.play(msgFromGraphics))};
-	//	//strcpy_s(msgToGraphics, code); // msgToGraphics should contain the result of the operation
-
-	//	/******* JUST FOR EREZ DEBUGGING ******/
-	//	int r = rand() % 10; // just for debugging......
-	//	msgToGraphics[0] = (char)(1 + '0');
-	//	msgToGraphics[1] = 0;
-	//	/******* JUST FOR EREZ DEBUGGING ******/
-
-	//	const char code[2] = { char('0' + chessGame.play(msgFromGraphics)) };
-	//	strcpy_s(msgToGraphics, code); // msgToGraphics should contain the result of the operation
-
-	//	// return result to graphics		
-	//	p.sendMessageToGraphics(msgToGraphics);   
-
-	//	// get message from graphics
-	//	msgFromGraphics = p.getMessageFromGraphics();
-	//}
-
-	////p.close();
-	//return 0;
+	
+	char msgToGraphics[1024];
+	// msgToGraphics should contain the board string accord the protocol
+	// YOUR CODE
+	strcpy_s(msgToGraphics, "rnbqkbnr"
+							"########"
+							"########"
+							"########"
+							"########"
+							"########"
+							"########"
+							"RNBQKBNR0"); // just example...
+	
+	p.sendMessageToGraphics(msgToGraphics);   // send the board string
+	// get message from graphics
+	string msgFromGraphics = p.getMessageFromGraphics();
+	while (msgFromGraphics != "quit")
+	{
+		// should handle the string the sent from graphics
+		// according the protocol. Ex: e2e4           (move e2 to e4)
+		
+		// YOUR CODE
+		//const char code[2] = {char( '0' + chessGame.play(msgFromGraphics))};
+		//strcpy_s(msgToGraphics, code); // msgToGraphics should contain the result of the operation
+		/******* JUST FOR EREZ DEBUGGING ******/
+		int r = rand() % 10; // just for debugging......
+		msgToGraphics[0] = (char)(1 + '0');
+		msgToGraphics[1] = 0;
+		/******* JUST FOR EREZ DEBUGGING ******/
+		const char code[2] = { char('0' + chessGame.play(msgFromGraphics)) };
+		strcpy_s(msgToGraphics, code); // msgToGraphics should contain the result of the operation
+		// return result to graphics		
+		p.sendMessageToGraphics(msgToGraphics);   
+		// get message from graphics
+		msgFromGraphics = p.getMessageFromGraphics();
+	}
+	//p.close();
+	return 0;
 }
